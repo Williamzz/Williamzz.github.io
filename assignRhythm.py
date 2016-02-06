e57fae688e10bf2b1d83c681db623d4948980f79
Riki Khorana <khorar@MacBook-Pro-3.wv.cc.cmu.edu>
2016-02-07 07:33:52 +0900

    updated

--- assignRhythm.py
+++ assignRhythm.py
@@ -1,4 +1,5 @@
 import string
+import copy
 
 # reduces common combinations of vowels of one syllable
 def onlyLetters(s):
@@ -34,6 +35,8 @@ def syllableCount(word):
     if word.endswith("e") and len(word)>2:
         if word[-3] in ["a", "e", "i", "o", "u", "n", "l"]:
             count -= 1
+    if word.endswith("asses"):
+        count += 1
     if count == 0:
         count += 1
     return count
@@ -50,10 +53,18 @@ def lineSyllables(line):
 
 def totalSyllables(lyrics):
     lyricSyllables = 0
-    lines = lyrics.splitlines()
+    lines = lyrics.split("/")
     for line in lines:
         lyricSyllables += lineSyllables(line)[0]
     return lyricSyllables
+
+def assignRhythm(lyrics):
+    beats = []
+    lines = lyrics.split("/")
+    for line in lines:
+        beats += lineSyllables(line)[1:]
+        beats += [0]
+    return beats
 
 ##############################################################
 ####    0 ----> new lines                               ######
@@ -64,11 +75,47 @@ def totalSyllables(lyrics):
 ####    etc...                                          ######
 ##############################################################
 
-def assignRhythm(lyrics):
-    beats = []
-    lines = lyrics.splitlines()
-    for line in lines:
-        beats += lineSyllables(line)[1:]
-        beats += [0]
-    return beats
+# returns list with 32 ints each representing a quarter beat in a measure
+def lyricsModification(lyrics):
+    rhythm = copy.copy(assignRhythm(lyrics))
+    zeroInsert = 3
+    while len(rhythm) < 32:
+        index = 0
+        copyRhythm = copy.copy(rhythm)
+        for i in copyRhythm:
+            index += 1
+            if len(rhythm) == 32:
+                break
+            elif i == 6:
+                rhythm.remove(i)
+                rhythm.insert(index, 3)
+                rhythm.insert(index, 3)
+            elif i == 5:
+                rhythm.remove(i)
+                rhythm.insert(index, 3)
+                rhythm.insert(index, 2)
+            elif i == 4:
+                rhythm.remove(i)
+                rhythm.insert(index, 2)
+                rhythm.insert(index, 2)
+        if len(rhythm) == 32:
+            break
+        else: 
+            rhythm += [0]
+
+    return rhythm
+
+def run(lyrics):
+    if (len(assignRhythm(lyrics)) > 32):
+        print ("your creation is %d words too long!" % (len(assignRhythm(lyrics))-32))
+        return None
+    else: 
+        return lyricsModification(lyrics)
+
+
+
+
+
+
+
 
